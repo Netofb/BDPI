00fa052077fd6b9621a68211f2dc4a244ded716e
Fabio Barros lima <106633969+Netofb@users.noreply.github.com>
2025-03-16 18:26:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ const anoAtual = new Date().getFullYear();
 
 
 
-    document.getElementById("cartao_sus").addEventListener("input", function() {
-        let erro = document.getElementById("erro");
-        if (this.value.length === 15) {
-            erro.style.display = "none";
-        } else {
-            erro.style.display = "block";
-            erro.style.position = "absolute"
-            erro.style.marginTop = "-3rem"
-        }
+    document.querySelectorAll("input[name='cartao_sus']").forEach(input => {
+        input.addEventListener("input", function() {
+            let erro = this.nextElementSibling; // Pega o elemento de erro próximo ao input
+            if (this.value.length === 15) {
+                erro.style.display = "none";
+            } else {
+                erro.style.display = "block";
+                erro.style.position = "absolute";
+                erro.style.marginTop = "9rem";
+            }
+        });
     });
